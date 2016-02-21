029329917afdc3cf81e1a263539e14c0401c0c57
Vadym Yatsyuk <vasp3d@gmail.com>
2016-02-21 07:56:43 +0100

    don't seed for now

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,6 +6,8 @@ import Question from './../models/Question'
 import questions from './seedQuestions'
 
 export default function () {
+  return; // do not seed for now
+
   Question.find({})
     .removeAsync()
     .then(() => {
